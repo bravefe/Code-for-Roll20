f6aabe73b877fec063b9fa18f3564563037e7e80
bravefe <patcharak.plipa@mail.kmutt.ac.th>
2026-03-08 17:05:44 +0700

    new file:   deck_png/roll-1.gif 	modified:   draw.js

--- draw.js
+++ draw.js
@@ -3,7 +3,62 @@ on('chat:message', function(msg) {
 
         let roll = randomInteger(16);
 
-        if (roll <= 15) {
+        if (roll === 1) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 2) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+        if (roll === 3) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 4) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 5) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 6) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 7) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 8) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+        
+        if (roll === 9) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 10) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 11) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 12) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 13) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 14) {
+            sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
+        }
+
+        if (roll === 15) {
             sendChat("Deck", `<img src="https://cdn.dribbble.com/userupload/18524820/file/original-a3a7dd07d2480b290441a33937a886d0.gif">`);
         }
 
